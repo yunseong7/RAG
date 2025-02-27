c16b167d0c972ae3eed9a8700b66f6cce5b6ebdb
yunseong7 <50496098+yunseong7@users.noreply.github.com>
2025-02-28 02:19:38 +0900

    Add files via upload

--- index.js
+++ index.js
@@ -8,15 +8,13 @@ $(document).ready(function(){
     var pwd = $("#pwd").val();
     var right = $("input[name='right']:checked").val();
 
-    console.log(right, id, pwd);
-
     if(id === "user" && pwd === "1234" && right === "user"){
       $(".inputBtn p").css("display","none");
-      location.href = "/user/index.html";
+      location.href = "user/index.html";
     }
     else if(id === "admin" && pwd === "1234" && right === "admin"){
       $(".inputBtn p").css("display","none");
-      location.href = "/admin/index.html";
+      location.href = "admin/index.html";
     }
     else{
       $(".inputBtn p").css("display","block");
